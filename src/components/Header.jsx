765040219bcc2df210f828da637043c43d7743d7
Fernando José da Silva <fernando.dev21@outlook.com>
2023-09-28 23:57:59 -0300

    n1 UX

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import logo from '../assets/image/logo.png'
 import logo2 from '../assets/image/logo-escrita.png'
-import redes from '../assets/image/socialMedia.jpg'
+import redes from '../assets/image/facebook.png'
+import redes_2 from '../assets/image/instagran.png'
+import redes_3 from '../assets/image/whatsApp.png'
 
 import './Header.css'
 const Header = () => {
@@ -9,12 +11,18 @@ const Header = () => {
     
     <header>
         <img src={logo} className="logo" alt="LOGO DA PAGINA ESCRITO AFFLECKS HQS" />
-        <img src={logo2} className="logo2" alt="LOGO DA PAGINA ESCRITO AFFLECKS HQS" />      
-        <img src={redes} className="redes" alt="LOGO DA PAGINA ESCRITO AFFLECKS HQS" />      
-    </header>
+        <img src={logo2} className="logo2" alt="LOGO DA PAGINA ESCRITO AFFLECKS HQS" />  
+
+        <div className='redes'>  
+          <img src={redes} className="redes" alt="LOGO DA PAGINA ESCRITO AFFLECKS HQS" />               
+          <img src={redes_2} className="redes" alt="LOGO DA PAGINA ESCRITO AFFLECKS HQS" />            
+          <img src={redes_3} className="redes" alt="LOGO DA PAGINA ESCRITO AFFLECKS HQS" />             
+        </div>  
+  
+    </header>   
 
   )
-}
+} 
 
 export default Header
 
