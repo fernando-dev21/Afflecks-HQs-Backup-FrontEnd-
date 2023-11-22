afd2acca399ae0286c2baeb0d28efd57b0e09aba
Fernando José da Silva <fernando.dev21@outlook.com>
2023-11-22 02:08:40 -0300

    produto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,12 @@ import hq3 from '../assets/image/hq3.jpg';
 import hq4 from '../assets/image/hq4.jpg';
 import hq5 from '../assets/image/hq5.jpg';
 import hq6 from '../assets/image/hq6.jpg';
+import hq7 from '../assets/image/hq7.jpg';
+import hq8 from '../assets/image/hq8.jpg';
+import hq9 from '../assets/image/hq9.png';
+import hq10 from '../assets/image/hq10.jpg';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle';
+
 
 const Home = () => {
   const [fontSize, setFontSize] = useState(16);
@@ -21,54 +24,58 @@ const Home = () => {
     setFontSize(fontSize - 1);
   };
 
+  const produtosIniciais = [
+    { id: 1, imagem: hq2, nome: 'Batman O cavaleiro das trevas', preco: 'R$ 29,99', adicionado: false },
+    { id: 2, imagem: hq1, nome: 'Superman', preco: 'R$ 29,99', adicionado: false },
+    { id: 3, imagem: hq4, nome: 'Os Vingadores', preco: 'R$ 29,99', adicionado: false },
+    { id: 4, imagem: hq3, nome: 'O Espetacular Homem Aranha', preco: 'R$ 29,99', adicionado: false },
+    { id: 5, imagem: hq5, nome: 'Batman Knightfall', preco: 'R$ 29,99', adicionado: false },
+    { id: 6, imagem: hq6, nome: 'Watchmen', preco: 'R$ 29,99', adicionado: false },
+    { id: 7, imagem: hq7, nome: 'Watchmen', preco: 'R$ 29,99', adicionado: false },
+    { id: 8, imagem: hq8, nome: 'Watchmen', preco: 'R$ 29,99', adicionado: false },
+    { id: 9, imagem: hq9, nome: 'Watchmen', preco: 'R$ 29,99', adicionado: false },
+    { id: 10, imagem: hq10, nome: 'Watchmen', preco: 'R$ 29,99', adicionado: false },
+
+
+  ];
+
+  const [produtos, setProdutos] = useState(produtosIniciais);
+
+  const handleClick = (id) => {
+    setProdutos((prevProdutos) =>
+      prevProdutos.map((produto) =>
+        produto.id === id ? { ...produto, adicionado: !produto.adicionado } : produto
+      )
+    );
+  };
+
   return (
     <main>
-      
       <div className="main">
+        {/* ... Controles de fonte ... */}
 
-      <div>
-        <button onClick={increaseFontSize}><strong>Aumentar fonte</strong></button>
-        <button onClick={decreaseFontSize}><strong>Diminuir fonte </strong></button>        
-      </div>
-      
-        <div className="produto" style={{ fontSize: `${fontSize}px` }}>
-          <img src={hq2} className="logo" alt="IMAGEM DA HQ DO BATMAN O CAVALEIROS DAS TREVAS" />
-          <h3 style={{ fontSize: `${fontSize}px` }}> Batman O cavaleiro das trevas</h3>
-          <p style={{ fontSize: `${fontSize}px` }}>R$ 29,99</p>
-          <button>Adicionar ao carrinho</button>
+        <div className="produtos-container">
+          {produtos.map((produto) => (
+            <div key={produto.id} className="produto" style={{ fontSize: `${fontSize}px` }}>
+              <img src={produto.imagem} className="logo" alt={`IMAGEM DA HQ DE ${produto.nome}`} />
+              <h3 style={{ fontSize: `${fontSize}px` }}>{produto.nome}</h3>
+              <p style={{ fontSize: `${fontSize}px` }}>{produto.preco}</p>
+
+              <div>
+                <button
+                  onClick={() => handleClick(produto.id)}
+                  style={{
+                    backgroundColor: produto.adicionado ? 'green' : '',
+                    color: produto.adicionado ? 'white' : 'black',
+                  }}
+                >
+                  {produto.adicionado ? 'Adicionado ao carrinho' : 'Adicionar ao carrinho'}
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-      <div className="produto" style={{ fontSize: `${fontSize}px` }}>
-        <img src={hq1} className="logo" alt="IMAGEM DA HQ DO SUPERMAN NÚMERO 4" />
-        <h3 style={{ fontSize: `${fontSize}px` }}> Superman </h3>
-        <p  style={{ fontSize: `${fontSize}px` }}>R$ 29,99</p>
-        <button>Adicionar ao carrinho</button>
-      </div>
-      <div className="produto" style={{ fontSize: `${fontSize}px` }}>
-        <img src={hq4} className="logo" alt="IMAGEM DA HQ DOS VINGADORES" />
-        <h3 style={{ fontSize: `${fontSize}px` }}>Os Vingadores</h3>
-        <p  style={{ fontSize: `${fontSize}px` }}>R$ 29,99</p>
-        <button>Adicionar ao carrinho</button>
-      </div>
-      <div className="produto" style={{ fontSize: `${fontSize}px` }}>
-        <img src={hq3} className="logo" alt="IMAGEM DA HQ DO AMAZING SPIDERMAN" />
-        <h3 style={{ fontSize: `${fontSize}px` }}> O Espetacular Homen Aranha</h3>
-        <p  style={{ fontSize: `${fontSize}px` }}>R$ 29,99</p>
-        <button>Adicionar ao carrinho</button>
-      </div>
-      <div className="produto" style={{ fontSize: `${fontSize}px` }}>
-        <img src={hq5} className="logo" alt="IMAGEM DO HQ BATMAN KNIGHTFALL" />
-        <h3 style={{ fontSize: `${fontSize}px` }}> Batman Knightfall</h3>
-        <p  style={{ fontSize: `${fontSize}px` }}>R$ 29,99</p>
-        <button>Adicionar ao carrinho</button>
-      </div>
-      <div className="produto" style={{ fontSize: `${fontSize}px` }}>
-        <img src={hq6} className="logo" alt="IMAGEM DA HQ DO WATCHMEN" />
-        <h3 style={{ fontSize: `${fontSize}px` }}> Watchmen</h3>
-        <p  style={{ fontSize: `${fontSize}px` }}>R$ 29,99</p>
-        <button>Adicionar ao carrinho</button>
-      </div>
-     
     </main>
   );
 };
